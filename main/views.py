f8daace4eb6ace073a117186a0eacfe3cbdec78d
Ваше Имя <you@example.com>
2024-12-28 16:15:12 +0300

    ini de beninging

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from main.forms import SignInForm
+from main.forms import *
 
 # Create your views here.
 def login(request):
@@ -7,3 +7,9 @@ def login(request):
     context={}
     context['form'] = form
     return render(request=request, template_name='login.html', context=context)
+
+def register(request):
+    form = RegisterForm()
+    context={}
+    context['form'] = form
+    return render(request=request, template_name='register.html', context=context)
