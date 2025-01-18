fb5e869d9e9ed8b2853210ad23c9b7e90c0840de
Ваше Имя <you@example.com>
2025-01-18 15:52:32 +0300

    '

--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('logout/', logout_view, name='logout'),
-    path('posts/', main_posts, name='posts')
+    path('posts/', main_posts, name='posts'),
 ]
